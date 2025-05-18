ef89dc748dcda8a81d50bf0df55dd26a07120983
jonasr1 <yatosama924@gmail.com>
2025-05-18 14:04:41 -0300

    feat: implement recipe generation and display in home and recipe view pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpRequest, HttpResponse
+
+from recipes.utils.recipes.factory import make_recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Jonas'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request: HttpRequest, id: int) -> HttpResponse:
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Jonas'
+        'recipe': make_recipe(), 'is_detail_page': True
     })
