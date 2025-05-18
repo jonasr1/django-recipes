d1257ae291a0ce83263415c2c4d819dd0caea9b2
jonasr1 <yatosama924@gmail.com>
2025-05-18 10:05:25 -0300

    feat: refactor templates and styles using  Inheritance in templates

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Jonas'
     })
+
+
+def recipe(request: HttpRequest, id: int) -> HttpResponse:
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Jonas'
+    })
