2975be08892e7deb85dc0002c82ace89cd1ad263
jonasr1 <yatosama924@gmail.com>
2025-05-25 17:19:13 -0300

    feat: update recipe model fields, add category view, and enhance templates for better recipe display

--- recipes/models.py
+++ recipes/models.py
@@ -17,14 +17,15 @@ class Recipe(models.Model):
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.CharField(max_length=65)
     servings_unit = models.CharField(max_length=65)
-    praparation_steps = models.TextField()
-    praparation_steps_is_html = models.BooleanField(default=False)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 
-from recipes.utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
-    })
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
+
+
+def category(request: HttpRequest, category_id: int) -> HttpResponse:
+    recipes = get_list_or_404(
+        Recipe.objects.filter(is_published=True, category__id=category_id).order_by('-id')
+    )
+    category = recipes[0].category
+    title = f'{category.name if category else 'Unknown'}'
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes, 'title': f'{title} - Category '})
 
 
 def recipe(request: HttpRequest, id: int) -> HttpResponse:
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(), 'is_detail_page': True
+        'recipe': recipe, 'is_detail_page': True
     })
