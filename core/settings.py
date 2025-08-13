e636d75ac45835505cac7c79d2f0d3d52ed2c8b0
jonasr1 <yatosama924@gmail.com>
2025-08-13 13:39:50 -0300

    feat(messages): add message display functionality and styles for user notifications

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 from decouple import config
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "message-debug",
+    messages.INFO: "message-info",
+    messages.SUCCESS: "message-success",
+    messages.WARNING: "message-warning",
+    messages.ERROR: "message-error"
+}
