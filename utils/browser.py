2ad6f48ebef7e954438e2f9a59075179051a5f50
jonasr1 <yatosama924@gmail.com>
2025-08-26 20:05:54 -0300

    chore(tests): add SELENIUM_HEADLESS support and update .env-example

--- utils/browser.py
+++ utils/browser.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from decouple import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -11,10 +12,11 @@ CHROMEDRIVER_PATH = ROOT_PATH / "bin" / CHROMEDRIVER_NAME
 
 def make_chrome_browser(*options: str) -> WebDriver:
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "/usr/bin/brave-browser"
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+    if selenium_headless := config("SELENIUM_HEADLESS", default=""):
+        chrome_options.add_argument(selenium_headless)
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     return webdriver.Chrome(service=chrome_service, options=chrome_options)
 
@@ -22,4 +24,4 @@ def make_chrome_browser(*options: str) -> WebDriver:
 if __name__ == "__main__":
     browser = make_chrome_browser()
     browser.get("http://www.udemy.com/")
-    browser.close()
+    browser.quit()
