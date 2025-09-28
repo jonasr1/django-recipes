ab73fcbb15590a0c4aef176a8275b7118c0622af
jonasr1 <yatosama924@gmail.com>
2025-09-28 15:38:06 -0300

    feat(admin): improve RecipeAdmin with filters, search and ordering

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,13 +3,20 @@ from django.contrib import admin
 from recipes.models import Category, Recipe
 
 
-class CategoryAdmin(admin.ModelAdmin): # type: ignore
+class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
 @admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin): # type: ignore
-    ...
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "is_published", "author")
+    list_display_links = ("title",)
+    search_fields = ("id", "title", "description", "slug", "preparation_steps")
+    list_filter = ("category", "author", "is_published", "preparation_steps_is_html")
+    list_per_page = 10
+    list_editable = ("is_published",)
+    ordering = ("-id",)
+    prepopulated_fields = {"slug": ("title",)}  # noqa: RUF012
 
 
 admin.site.register(Category)
