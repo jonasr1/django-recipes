e840c43c7b9db7e2618d1fc21cbca28a81ad69cf
JonasR <115326489+jonasr1@users.noreply.github.com>
2025-10-29 18:34:06 -0300

    Dev/jonas (#3)
    * style(models): fix trailing comma in Recipe model
    
    * feat(admin): improve RecipeAdmin with filters, search and ordering
    
    * feat(authors): implement user dashboard with recipes list
    
    * feat(authors): add recipe editing functionality and improve dashboard layout
    
    * feat(authors): enhance RegisterForm to use typed User and fix password handling
    
    * refactor: :recycle: move choices to Recipe model
    
    * feat(authors): handle file uploads in RegisterForm and dashboard; update CSS and form attributes
    
    * feat(authors): implement recipe management features including creation, editing, and deletion; enhance dashboard layout and validation
    
    * feat(recipes): add model methods for slug and absolute URL
    
    - Add `get_absolute_url` and slug generation to Recipe model
    - Update recipe templates to use the new URL method
    - Move `views.py` into the views package for better organization

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -43,3 +45,12 @@ class Recipe(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs) -> None: # noqa
+        if not self.slug:
+            slug = f"{slugify}"
+            self.slug = slug
+        return super().save(*args, **kwargs)
+
+    def get_absolute_url(self) -> str:
+        return reverse("recipes:recipe", kwargs={"pk": self.pk})
