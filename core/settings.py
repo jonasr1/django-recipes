b3256140636173ce3a29ec26de3f1e0e5267cbed
jonasr1 <yatosama924@gmail.com>
2025-12-18 14:44:59 -0300

    feat(settings): update environment configuration for Cloudflare R2 and refactor database settings feat(storages): add custom MediaStorage class for S3Boto3Storage fix(recipe.html): correct link to recipe's absolute URL in template chore(dependencies): add boto3 to project dependencies in pyproject.toml

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "utils",
     "django.contrib.staticfiles",
+    "storages",
     "recipes",
     "authors",
 ]
@@ -121,12 +123,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -136,7 +143,23 @@ STATICFILES_DIRS = [
     BASE_DIR / "base_static",
 ]
 
-MEDIA_URL = "/media/"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME", default="auto")
+AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
+
+AWS_S3_ADDRESSING_STYLE = "path"
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN")
+
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
@@ -151,13 +174,3 @@ MESSAGE_TAGS = {
     messages.WARNING: "message-warning",
     messages.ERROR: "message-error",
 }
-
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
-AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
-
-DEFAULT_FILE_STORAGE = config("DEFAULT_FILE_STORAGE")
-
-AWS_S3_FILE_OVERWRITE = config("AWS_S3_FILE_OVERWRITE", cast=bool)
